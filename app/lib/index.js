8836b6836bcb31ef099564cb9f516ff8fcce11ea
Alberto Rocha <betoharres@gmail.com>
2018-08-29 01:53:46 -0300

    Add new schedules JSON structure

--- app/lib/index.js
+++ app/lib/index.js
@@ -1,3 +1,6 @@
-import { fromJS } from 'immutable'
-import busSchedules from './bus_schedules.json'
-export default fromJS(busSchedules)
+import { fromJS, isKeyed } from 'immutable'
+import busSchedules from './20180826151756.json'
+export default fromJS(busSchedules, (key, value) => {
+  // OrderedMap so the schedule keys keep in order
+  return isKeyed(value) ? value.toOrderedMap() : value.toList()
+})

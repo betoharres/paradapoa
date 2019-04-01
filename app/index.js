0ccaf68f20856f6cab5e6dd91e735747efa02d63
Alberto Rocha <betoharres@gmail.com>
2019-04-01 01:41:59 -0300

    wip

--- app/index.js
+++ app/index.js
@@ -1,23 +1,24 @@
 import React, { Component } from 'react'
 import { View } from 'react-native'
-import { createStackNavigator } from 'react-navigation'
+import { createAppContainer, createStackNavigator } from 'react-navigation';
 import { HomeContainer, BusDetailsContainer } from './containers'
 
 // https://github.com/facebook/react-native/issues/18868
 import { YellowBox } from 'react-native'
 YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader', 'Class RCTCxxModule'])
 
-const RootStack = createStackNavigator({
+const AppNavigator = createStackNavigator({
   Home: HomeContainer,
   BusDetails: BusDetailsContainer,
 });
+const AppContainer = createAppContainer(AppNavigator);
 
 export default class App extends Component<{}> {
 
   render() {
     return (
       <View style={{flex: 1}}>
-        <RootStack />
+        <AppContainer />
       </View>
     )
   }
